3baedffb2cc1beff3b83affdf8aa49c0367b5f36
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-19 21:54:10 -0600

    Add function that reverses string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,22 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // let reversed = ''
+
+  // for (let character of str) {
+  //   reversed = character + reversed
+  // }
+
+  // return reversed
+
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('')
+
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
+
