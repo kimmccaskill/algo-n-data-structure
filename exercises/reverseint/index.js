c1ecde17d237beff965223367c29775c8a289481
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-19 21:54:21 -0600

    Add function that reverses integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reverse = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('')
+  return parseInt(reverse) * Math.sign(n)
+}
 
 module.exports = reverseInt;
