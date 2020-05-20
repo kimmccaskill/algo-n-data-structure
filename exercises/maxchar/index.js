5e891bd4bb44d87eed7db4d26b8c5d5439a830f6
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-19 21:54:54 -0600

    Add function that finds max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charCount = {}
+  let max = 0
+  let maxChar = ''
+  for (let char of str) {
+    if (!charCount[char]) {
+      charCount[char] = 1
+    } else {
+      charCount[char]++
+    }
+  }
+  for (let char in charCount) {
+    if (charCount[char] > max) {
+      max = charCount[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
+}
 
 module.exports = maxChar;
